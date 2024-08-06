37e91d1409af0ec33bb3524177515d0ea0ea6b37
sasindum98 <sasindumihishan353@gmail.com>
2024-08-06 13:06:12 +0530

    searchBarUpdate

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ let tableBody = ` <tr>
         </tr>`
 
 let tes = fetch("https://restcountries.com/v3.1/all")
-.then((res) =>res.json())
-.then(data=>{
+    .then((res) => res.json())
+    .then(data => {
 
-    data.forEach(element =>{
-    tableBody+=` <tr>
+        data.forEach(element => {
+            tableBody += ` <tr>
     
     <td><h1>${element.name.common}</h1><br>
     Official Name : ${element.name.official}<br>
@@ -22,11 +22,38 @@ let tes = fetch("https://restcountries.com/v3.1/all")
 
     <td><img src="${element.flags.png}" alt=""></td>
 </tr>`
-        console.log(element.name.common);
+            console.log(element.name.common);
+
+        });
         
+
+        tblCountries.innerHTML = tableBody;
     });
 
-    tblCountries.innerHTML = tableBody;
+function searchCountry() {
+
+
+let userInput = document.getElementById("txtInput").value;
+
+
+let flagImg =  document.getElementById("flagImg");
+let name = document.getElementById("name");
+let officialName = document.getElementById("officialName");
+let region = document.getElementById("region");
+let area = document.getElementById("area");
+
+fetch(`https://restcountries.com/v3.1/name/${userInput}`)
+.then(res =>res.json())
+.then(data => {
+    data.forEach(obj =>{
+        console.log(obj);
+        flagImg.src=obj.flags.png
+        name.innerText=obj.name.common
+        officialName.innerText = obj.name.official
+        region.innerText= obj.region
+        area.innerText=obj.area
+        
+    })     
 })
 
-//https://restcountries.com/v3.1/all
+}
